768ef3e96d6976a267f2896922ed86dfffd9489d
MohammadAlamin99 <alamin.cse.99@gmail.com>
2025-08-06 14:32:05 +0600

    gsap

---
+++ main.js
@@ -0,0 +1,77 @@
+gsap.from(".page1 .box", {
+    scale: 0,
+    delay: 1,
+    duration: 1,
+})
+gsap.from(".page2 .box", {
+    scale: 0,
+    delay: 1,
+    duration: 1,
+    scrollTrigger: {
+        trigger: ".page2 .box",
+        start: "top center",
+    }
+})
+gsap.from(".page3 .box", {
+    opacity: "0",
+    duration: 1,
+    x: -1000,
+    scrollTrigger: {
+        trigger: ".page3 .box",
+        scroller: "body",
+        start: "top 60%",
+        end: "top 30%",
+        scrub: 1,
+    }
+})
+gsap.from(".page4 .box", {
+    opacity: "0",
+    duration: 2,
+    rotate: 720,
+    x: 500,
+    scrollTrigger: {
+        trigger: ".page4 .box",
+        scroller: "body",
+        start: "top 80%",
+        end: "top 10%",
+        scrub: 2,
+    }
+})
+
+
+gsap.to(".page5 h1", {
+    transform: "translateX(-200%)",
+    scrollTrigger: {
+        trigger: ".page5",
+        scroller: "body",
+        markers: true,
+        start: "top 0%",
+        end: "top -100%",
+        scrub: 2,
+        pin: true,
+    }
+})
+
+
+// svg 
+
+let Path = `M 10 100 Q 500 100 990 100`;
+const finalPath = `M 10 100 Q 500 100 990 100`;
+
+const main_div = document.querySelector(".svg-wrapper");
+
+main_div.addEventListener("mousemove", (dots) => {
+    Path = `M 10 100 Q ${dots.x}  ${dots.y} 990 100`;
+    gsap.to(".svg-wrapper svg path", {
+        duration:0.2,
+        attr: { d: Path },
+        ease: "power3.out",
+    })
+})
+main_div.addEventListener("mousemove", () => {
+    gsap.to(".svg-wrapper svg path", {
+        attr: { d: finalPath },
+        duration:1.5,
+        ease: "elastic.out(1,0.2)",
+    })
+})
